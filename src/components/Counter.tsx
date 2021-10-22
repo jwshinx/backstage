c76da68df9904c79c917f1497806a82e7251ea7c
joel shin <jwshin@gmail.com>
2021-10-22 00:52:32 -0700

    installed react testing library

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -6,7 +6,7 @@ export const Counter = () => {
   const [count, setCount] = useState(0)
   return (
     <div className={`counter ${styles.dark}`}>
-      <h3>Update the count and edit src/App.tsx, state is preserved</h3>
+      <h3>Counter</h3>
       <button
         className="btn btn-primary"
         onClick={() => setCount((c) => c + 1)}
