8a18d205e3312a766b650ee7e3c1e692f40c35b2
joel shin <jwshin@gmail.com>
2021-10-21 23:43:48 -0700

    yarn test command runs jest in watch mode

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -8,4 +8,11 @@ describe('Test Suite', () => {
   it('click on a button', () => {
     console.log(`+++> ha`)
   })
+
+  it('do another thing', () => {
+    console.log(`+++> another thing 1`)
+    expect.assertions(1)
+    expect(2 + 2).toBe(4)
+    console.log(`+++> another thing 2`)
+  })
 })
