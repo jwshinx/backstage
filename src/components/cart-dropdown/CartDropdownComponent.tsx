b97e2e9e1087b5b7ef9e777434500c1441e7b4cd
joel shin <jwshin@gmail.com>
2021-12-30 11:59:24 -0800

    worked in CartProvider into CartDropdownComponent

--- src/components/cart-dropdown/CartDropdownComponent.tsx
+++ src/components/cart-dropdown/CartDropdownComponent.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styles from './CartDropdownComponent.module.css'
 import CustomButton from '../../ui/button/CustomButtonComponent'
+import { CartContext } from '../../providers/CartProvider'
 
 export default function CartDropdownComponent() {
+  const { cartItems } = useContext(CartContext)
+
   return (
     <div className={styles['cart-dropdown']}>
-      <span>CartDropdownComponent</span>
-      <div className={styles['cart-items']}></div>
+      <div className={styles['cart-items']}>
+        {cartItems.length === 0 ? (
+          <p className={styles['empty-message']}>No items.</p>
+        ) : (
+          cartItems.map((item: any) => {
+            return <span key={item.name}>{item.name}</span>
+          })
+        )}
+      </div>
       <div className={styles['cart-button-area']}>
         <CustomButton isDisabled={false} type="submit">
           CHECKOUT
