28bbd550de0132d102033b81754d065bd5af1723
joel shin <jwshin@gmail.com>
2021-12-31 08:47:07 -0800

    CheckoutPageComponent

--- src/pages/checkout/CheckoutPageComponent.tsx
+++ src/pages/checkout/CheckoutPageComponent.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CartContext } from '../../providers/CartProvider'
+import { CartItem } from '../../providers/cart'
+import styles from './CheckoutPageComponent.module.css'
 
 export default function CheckoutPageComponent() {
+  const ctx = useContext(CartContext)
+  console.log(`+++> @joel CheckoutPageComponent ctx:`, ctx)
+
+  let cartItems: Array<CartItem> = []
+  if (ctx && ctx.cartItems !== undefined) {
+    cartItems = ctx.cartItems
+  }
+
+  console.log(`+++> @joel CheckoutPageComponent cartItems:`, cartItems)
+
   return (
-    <div>
-      <h2>CheckoutPageComponent</h2>
+    <div className={styles['checkout']}>
+      <div className={styles['checkout-header']}>
+        <div className={styles['header-block']}>
+          <span>Product</span>
+        </div>
+        <div className={styles['header-block']}>
+          <span>Description</span>
+        </div>
+        <div className={styles['header-block']}>
+          <span>Quantity</span>
+        </div>
+        <div className={styles['header-block']}>
+          <span>Price</span>
+        </div>
+        <div className={styles['header-block']}>
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map((item: CartItem) => {
+        return (
+          <div key={item.id}>
+            {item.name} - {item.quantity} - {item.price}
+          </div>
+        )
+      })}
     </div>
   )
 }
