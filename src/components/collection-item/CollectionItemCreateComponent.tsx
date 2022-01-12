c3a6fe7071bbf9578b22a21b1d1b08efa1e3c114
joel shin <jwshin@gmail.com>
2022-01-12 10:58:05 -0800

    added form validation

--- src/components/collection-item/CollectionItemCreateComponent.tsx
+++ src/components/collection-item/CollectionItemCreateComponent.tsx
@@ -3,41 +3,59 @@ import styles from './CollectionItemCreateComponent.module.css'
 
 const CollectionItemCreateComponent = () => {
   const [itemName, setItemName] = useState('')
+  const [itemNameIsTouched, setItemNameIsTouched] = useState(false)
+
   const [itemPrice, setItemPrice] = useState('')
+  const [itemPriceIsTouched, setItemPriceIsTouched] = useState(false)
+
   const [itemImageUrl, setItemImageUrl] = useState('')
+  const [itemImageUrlIsTouched, setItemImageUrlIsTouched] = useState(false)
 
   const itemNameChangeHandler = (event: any) => {
-    console.log(`+++> itemNameChangeHandler click!`)
     setItemName(event.target.value)
   }
 
   const itemNameInputBlurHandler = () => {
-    console.log(`+++> itemNameInputBlurHandler click!`)
+    setItemNameIsTouched(true)
   }
 
   const itemPriceChangeHandler = (event: any) => {
-    console.log(`+++> itemPriceChangeHandler click!`)
     setItemPrice(event.target.value)
   }
 
   const itemPriceInputBlurHandler = () => {
-    console.log(`+++> itemPriceInputBlurHandler click!`)
+    setItemPriceIsTouched(true)
   }
 
   const itemImageUrlChangeHandler = (event: any) => {
-    console.log(`+++> itemImageUrlChangeHandler click!`)
     setItemImageUrl(event.target.value)
   }
 
   const itemImageUrlInputBlurHandler = () => {
-    console.log(`+++> itemImageUrlInputBlurHandler click!`)
+    setItemImageUrlIsTouched(true)
   }
 
   const formSubmitHandler = (event: any) => {
     event.preventDefault()
+
+    if (itemNameIsInvalid || itemPriceIsInvalid || itemImageUrlIsInvalid) {
+      return
+    }
     console.log(`+++> formSubmitHandler click!`, event)
     console.log(`+++> formSubmitHandler do submit!`)
   }
+
+  const itemNameIsInvalid = itemNameIsTouched && itemName.trim() === ''
+  const itemPriceIsInvalid = itemPriceIsTouched && itemPrice.trim() === ''
+  const itemImageUrlIsInvalid =
+    itemImageUrlIsTouched && itemImageUrl.trim() === ''
+
+  const formIsValid =
+    itemName.trim() !== '' &&
+    itemPrice.trim() !== '' &&
+    itemImageUrl.trim() !== ''
+
+  // console.log(`+++> CCC formIsValid:`, formIsValid)
 
   // name, imageUrl, price
   return (
@@ -55,6 +73,11 @@ const CollectionItemCreateComponent = () => {
               onChange={itemNameChangeHandler}
               onBlur={itemNameInputBlurHandler}
             />
+            <div className={styles['error-area']}>
+              <span className={styles['error-message']}>
+                {itemNameIsInvalid ? 'Please enter a value' : ''}
+              </span>
+            </div>
           </div>
 
           <div className={`form-group ${styles['input-area']}`}>
@@ -68,6 +91,11 @@ const CollectionItemCreateComponent = () => {
               onChange={itemPriceChangeHandler}
               onBlur={itemPriceInputBlurHandler}
             />
+            <div className={styles['error-area']}>
+              <span className={styles['error-message']}>
+                {itemPriceIsInvalid ? 'Please enter a value' : ''}
+              </span>
+            </div>
           </div>
 
           <div className={`form-group ${styles['input-area']}`}>
@@ -81,10 +109,17 @@ const CollectionItemCreateComponent = () => {
               onChange={itemImageUrlChangeHandler}
               onBlur={itemImageUrlInputBlurHandler}
             />
+            <div className={styles['error-area']}>
+              <span className={styles['error-message']}>
+                {itemImageUrlIsInvalid ? 'Please enter a value' : ''}
+              </span>
+            </div>
           </div>
 
           <br />
-          <button className="btn btn-primary">Submit</button>
+          <button disabled={!formIsValid} className="btn btn-primary">
+            Submit
+          </button>
         </form>
       </div>
     </div>
